85c76c86f3a4a0db612397c6148277e128cb27df
Oliver Günther <oliver.guenther@gg-net.de>
2014-11-11 18:04:02 +0100

    Copyright corrected

--- ui/persistence/rights/src/main/java/eu/ggnet/dwoss/rights/RightsListCell.java
+++ ui/persistence/rights/src/main/java/eu/ggnet/dwoss/rights/RightsListCell.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2014 pascal.perau
+ * Copyright (C) 2014 GG-Net GmbH - Oliver Günther
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
