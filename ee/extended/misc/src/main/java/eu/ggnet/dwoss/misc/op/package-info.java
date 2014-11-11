2b1af6e023af25be2e30b054d4663e0f25554bda
Pascal Perau <passi_03@gmx.de>
2014-11-11 11:02:07 +0100

    added GPL 3.0 license to ee.extended, ee-core and assembly

--- ee/extended/misc/src/main/java/eu/ggnet/dwoss/misc/op/package-info.java
+++ ee/extended/misc/src/main/java/eu/ggnet/dwoss/misc/op/package-info.java
@@ -1,11 +1,17 @@
-/**
- * Miscellaneous Operations.
- * Put here Operations that don't belong somewhere else.
- * Don't use a sub package.
- * If someone detects, that the misc or the misc.ui package becomes polluted, that means more than 7-10 Classes in the package, create a high priority issue with
- * a Title that indicates that the package need a cleanup.
- * That issue will be solved in a short talk where we all together choose, what will be sorted into packages and what will become a new project.
- * These decisions will be put into extra issues and handle accordingly.
- * <p>
+/* 
+ * Copyright (C) 2014 pascal.perau
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package eu.ggnet.dwoss.misc.op;
