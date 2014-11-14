20cbf1aa5b2b7cf56d80280d9a1e40cde689d00d
Pascal Perau <passi_03@gmx.de>
2014-11-14 16:05:39 +0100

    implemented the subscribtion view new in jfx with some validation

--- assembly/client-sample/src/main/java/eu/ggnet/dwoss/assembly/sample/RunClientFx.java
+++ assembly/client-sample/src/main/java/eu/ggnet/dwoss/assembly/sample/RunClientFx.java
@@ -18,22 +18,17 @@ package eu.ggnet.dwoss.assembly.sample;
 
 import java.awt.EventQueue;
 import java.awt.Toolkit;
-import java.io.*;
 import java.net.*;
+import java.util.Objects;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.Timeline;
 import javafx.application.*;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import org.openide.util.Lookup;
 
-import eu.ggnet.dwoss.common.ExceptionUtil;
 import eu.ggnet.dwoss.common.UnhandledExceptionCatcher;
 import eu.ggnet.dwoss.mandator.MandatorSupporter;
 import eu.ggnet.dwoss.mandator.api.value.Mandator;
@@ -94,86 +89,18 @@ public class RunClientFx extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        showSuscribtion();
-
+//        showSuscribtion();
+        FXMLLoader loader = new FXMLLoader(SubscribtionController.loadFxml());
+        Pane root = (Pane)loader.load();
+        SubscribtionController controller = Objects.requireNonNull(loader.getController());
+        Stage newStage = new Stage();
+        newStage.setTitle("Willkommen in der Testversion der DWOSS");
+        newStage.setScene(new Scene(root));
+        newStage.setResizable(false);
+        controller.setStage(newStage);
+        newStage.showAndWait();
         EventQueue.invokeLater(() -> {
             swingClient.show("(Sample) - Mandant:" + lookup(MandatorSupporter.class).loadMandator().getCompany().getName(), getParameters());
         });
-    }
-
-    private void sendPost(String name, String email, String message) {
-        try {
-            String url = "http://gg-net.de/registerDw/register-email.php";
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection)obj.openConnection();
-            //add reuqest header
-            con.setRequestMethod("POST");
-            String urlParameters = "name=" + URLEncoder.encode(name, "UTF-8") + "&email=" + URLEncoder.encode(email, "UTF-8")
-                    + "&message=" + URLEncoder.encode(message, "UTF-8");
-            // Send post request
-            con.setDoOutput(true);
-            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
-                wr.writeBytes(urlParameters);
-                wr.flush();
-            }
-            try (BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()))) {
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-            }
-        } catch (IOException ex) {
-            ExceptionUtil.show(null, ex);
-        }
-
-    }
-
-    private void showSuscribtion() {
-        Stage dialog = new Stage();
-        dialog.setTitle("Willkommen in der Testversion der DWOSS");
-        try {
-            dialog.getIcons().add(new Image(loadAppImage().openStream()));
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
-        dialog.setAlwaysOnTop(true);
-        dialog.setWidth(250);
-
-        Label infoText = new Label(""
-                + "Vielen Dank für ihr Interesse an  der Deutschen Warenwirtschaft Open Source Software (DWOSS).\n\n"
-                + "Dies ist eine Testversion, die bei jedem Start neue Testdaten generiert. "
-                + "Änderungen und von ihnen erstellte Daten gehen beim beenden der Applikation verloren.\n\n"
-                + "Bei Fragen, Anregunden oder weiterführende Informationen für Unterstützung seitens der GG-Net GmbH "
-                + "füllen sie gern die in diesem Fenter abgebildete Form aus und Schicken diese per Knopfdruck ab.");
-        infoText.setWrapText(true);        
-        
-        TextField emailField = new TextField();
-        emailField.setPromptText("Email");
-        TextField nameField = new TextField();
-        nameField.setPromptText("Ihr Name");
-        TextArea messageField = new TextArea();
-        messageField.setPromptText("Optionale Nachricht");
-        Button send = new Button("Senden");
-        send.setOnAction((eh) -> {
-            sendPost(nameField.getText(), emailField.getText(), messageField.getText());
-            dialog.close();
-        });
-
-        Button cancel = new Button("Überspringen");
-        cancel.setOnAction((e) -> dialog.close());
-
-        GridPane p = new GridPane();
-        p.setHgap(5);
-        p.setVgap(5);
-        p.add(infoText, 0, 0, 2, 1);
-        p.add(nameField, 0, 1);
-        p.add(emailField, 1, 1);
-        p.add(messageField, 0, 2, 2, 1);
-        p.add(new FlowPane(5, 0, send, cancel), 0, 3, 2, 1);
-        Scene scene = new Scene(p);
-        dialog.setScene(scene);
-        dialog.showAndWait();
     }
 }
