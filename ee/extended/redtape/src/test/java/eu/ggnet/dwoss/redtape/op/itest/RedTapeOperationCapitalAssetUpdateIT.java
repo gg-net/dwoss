55f9259b16ee6890fedd434aba2743ac3f837340
Oliver Günther <oliver.guenther@gg-net.de>
2017-06-16 20:32:47 +0200

    Completed on redtape.

--- ee/extended/redtape/src/test/java/eu/ggnet/dwoss/redtape/op/itest/RedTapeOperationCapitalAssetUpdateIT.java
+++ ee/extended/redtape/src/test/java/eu/ggnet/dwoss/redtape/op/itest/RedTapeOperationCapitalAssetUpdateIT.java
@@ -17,10 +17,8 @@ import eu.ggnet.dwoss.redtape.*;
 import eu.ggnet.dwoss.redtape.entity.*;
 import eu.ggnet.dwoss.redtape.op.itest.support.*;
 import eu.ggnet.dwoss.rules.DocumentType;
-import eu.ggnet.dwoss.rules.PositionType;
 import eu.ggnet.dwoss.stock.StockAgent;
 import eu.ggnet.dwoss.stock.entity.LogicTransaction;
-import eu.ggnet.dwoss.stock.entity.StockUnit;
 import eu.ggnet.dwoss.uniqueunit.entity.UniqueUnit;
 import eu.ggnet.dwoss.util.UserInfoException;
 
@@ -34,7 +32,7 @@ import static org.junit.Assert.*;
  * @author bastian.venz
  */
 @RunWith(Arquillian.class)
-public class RedTapeOperationCapitalAssetIT extends ArquillianProjectArchive {
+public class RedTapeOperationCapitalAssetUpdateIT extends ArquillianProjectArchive {
 
     private long capitalAssestCustomer;
 
@@ -85,7 +83,6 @@ public class RedTapeOperationCapitalAssetIT extends ArquillianProjectArchive {
     public void testUpdate() throws UserInfoException {
         UniqueUnit uu = receiptGenerator.makeUniqueUnit();
         uu.setPrice(CUSTOMER, 50., "JunitTestPrice");
-
         //Generate Dossier
         Dossier dos = redTapeWorker.create(capitalAssestCustomer, false, "Me");
         assertThat(dos.getActiveDocuments()).isNotEmpty();
@@ -117,51 +114,6 @@ public class RedTapeOperationCapitalAssetIT extends ArquillianProjectArchive {
         assertTrue(stockAgent.findAllEager(LogicTransaction.class).get(0).getUnits().size() == 1);
 
         assertEquals("Ammount of Documents", 2, redTapeAgent.findAll(Document.class).size());
-    }
-
-    @Test
-    public void testCancel() throws UserInfoException {
-        UniqueUnit uu = receiptGenerator.makeUniqueUnit();
-        uu.setPrice(CUSTOMER, 50., "JunitTestPrice");
-
-        //Generate Dossier
-        Dossier dos = redTapeWorker.create(capitalAssestCustomer, false, "Me");
-        Document doc = dos.getActiveDocuments(DocumentType.CAPITAL_ASSET).get(0);
-        assertTrue(doc.equalsContent(dos.getActiveDocuments(DocumentType.CAPITAL_ASSET).get(0)));
-
-        //Commit explicit date to document for assertion
-        doc = supportBean.changeActual(doc, new GregorianCalendar(2012, 3, 15).getTime());
-
-        //Create Positions
-        doc.append(NaivBuilderUtil.unit(uu));
-        doc.append(NaivBuilderUtil.comment("Comment", "A nice comment"));
-
-        //add units to LogicTransaction
-        unitOverseer.lockStockUnit(dos.getId(), uu.getIdentifier(UniqueUnit.Identifier.REFURBISHED_ID));
-
-        //update document
-        Date date = doc.getActual();
-        doc = redTapeWorker.update(doc, null, "Me");
-
-        assertEquals("Same actual dates expected", date, doc.getActual());
-
-        assertEquals("Only one Active Document expected", 1, doc.getDossier().getActiveDocuments().size());
-        assertTrue(stockAgent.findAllEager(LogicTransaction.class).get(0).getUnits().size() == 1);
-
-        assertEquals("Ammount of Documents", 2, redTapeAgent.findAll(Document.class).size());
-
-        // Now Cancel it
-        doc.add(Document.Condition.CANCELED);
-
-        doc = redTapeWorker.update(doc, null, "tester");
-
-        assertTrue("Document doesent contain the Condition Canceled " + doc, doc.getConditions().contains(Document.Condition.CANCELED));
-
-        for (Position position : doc.getPositions(PositionType.UNIT).values()) {
-            StockUnit su = stockAgent.findStockUnitByUniqueUnitIdEager(position.getUniqueUnitId());
-            assertNull(su + " should not have LogicTransaction", su.getLogicTransaction());
-        }
-
     }
 
 }
