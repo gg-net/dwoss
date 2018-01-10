48ec0825c4ef30081c8ae686865677432c8c3040
jacob.weinhold <jacob.weinhold@gg-net.de>
2018-01-10 17:04:29 +0100

    moving packeds

--- ui/persistence/customer/src/main/java/eu/ggnet/dwoss/customer/ui/neo/mainView/MainControllerDto.java
+++ ui/persistence/customer/src/main/java/eu/ggnet/dwoss/customer/ui/neo/mainView/MainControllerDto.java
@@ -16,36 +16,21 @@
  */
 package eu.ggnet.dwoss.customer.ui.neo.mainView;
 
-import java.net.URL;
 import java.util.*;
-
-import javafx.collections.FXCollections;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
 
 import eu.ggnet.dwoss.customer.entity.Customer.ExternalSystem;
 import eu.ggnet.dwoss.customer.entity.Customer.Source;
 import eu.ggnet.dwoss.customer.entity.*;
-import eu.ggnet.dwoss.customer.ui.CustomerTask;
 import eu.ggnet.dwoss.rules.CustomerFlag;
-import eu.ggnet.saft.Ui;
-import eu.ggnet.saft.api.ui.ClosedListener;
-import eu.ggnet.saft.api.ui.FxController;
-import eu.ggnet.saft.core.ui.FxSaft;
 
 import lombok.Getter;
 import lombok.Setter;
 
 /**
- * FXML Controller class
  *
- * @author jens.papenhagen
+ * @author jacob.weinhold
  */
-public class CustomerExpandedController implements Initializable, FxController, ClosedListener {
-
-    private final CustomerTask LOADING_TASK = new CustomerTask();
+public class MainControllerDto {
 
     @Getter
     @Setter
@@ -81,44 +66,23 @@ public class CustomerExpandedController implements Initializable, FxController, 
 
     @Getter
     @Setter
-    private String keyAccounter;  // Null is ok.
+    private String keyAccounter;
 
     @Getter
     @Setter
     private String comment;
 
-    @FXML
-    private TextField keyAccounterTextField;
-
-    @FXML
-    private TextArea commentTextArea;
-
-    @FXML
-    private ComboBox<Source> sourceComboBox;
-
-    @FXML
-    private VBox flagVbox;
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-
-        Ui.progress().observe(LOADING_TASK);
-        Ui.exec(LOADING_TASK);
+    public static void setControllerState(CustomerExpandedController controller, MainControllerDto dto) {
+        controller.setId(dto.getId());
+        controller.setComment(dto.getComment());
+        controller.setCompanies(dto.getCompanies());
+        controller.setContacts(dto.getContacts());
+        controller.setFlags(dto.getFlags());
+        controller.setId(dto.getId());
+        controller.setKeyAccounter(dto.getKeyAccounter());
+        controller.setMandatorMetadata(dto.getMandatorMetadata());
+        controller.setOptLock(dto.getOptLock());
+        controller.setSource(dto.getSource());
 
     }
-
-    @Override
-    public void closed() {
-        FxSaft.dispatch(() -> {
-            if ( LOADING_TASK.isRunning() ) LOADING_TASK.cancel();
-            return null;
-        });
-    }
-
-    private void setFlagVboxUp() {
-
-        flagVbox.getChildren().add(new ChoiceBox<CustomerFlag>(FXCollections.observableArrayList(Arrays.asList(CustomerFlag.values()))));
-
-    }
-
 }
