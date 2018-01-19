ed67faa157b101aadd132dd2fe2125923447e858
Jens Papenhagen <jenspapenhagen@users.noreply.github.com>
2018-01-19 16:54:26 +0100

    Customer adding toSimple() UTest

--- ee/persistence/customer/src/main/java/eu/ggnet/dwoss/customer/entity/Customer.java
+++ ee/persistence/customer/src/main/java/eu/ggnet/dwoss/customer/entity/Customer.java
@@ -66,7 +66,7 @@ public class Customer implements Serializable {
         COMPANY,
         ADDRESS
     }
-    
+
     @AllArgsConstructor
     @Getter
     public static enum Source {
@@ -75,19 +75,19 @@ public class Customer implements Serializable {
         SOPO_STORE("Sonderposten Store"),
         SOPO_ONLINE("Sonderposten Online"),
         ONEADO("Oneado Online Shop");
-        
+
         private final String name;
     }
-    
+
     public static enum ExternalSystem {
         SAGE, LEXWARE
     }
-    
+
     @Id
     @Getter
     @GeneratedValue
     private long id;
-    
+
     @Getter
     @Version
     private short optLock;
@@ -114,22 +114,22 @@ public class Customer implements Serializable {
     @NotNull
     @OneToMany(cascade = ALL)
     private List<MandatorMetadata> mandatorMetadata = new ArrayList<>();
-    
+
     @Getter
     @NotNull
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<CustomerFlag> flags = new HashSet<>();
-    
+
     @Getter
     @Setter
     private Source source;
-    
+
     @Getter
     @NotNull
     @ElementCollection(fetch = FetchType.EAGER)
     @MapKeyEnumerated
     private Map<ExternalSystem, String> additionalCustomerIds = new EnumMap<>(ExternalSystem.class);
-    
+
     @Getter
     @Setter
     private String keyAccounter;  // Null is ok.
@@ -141,19 +141,19 @@ public class Customer implements Serializable {
     @Field
     @Boost(0.5f)
     private String comment;
-    
+
     @Getter
     @Transient // Will be in the entity model later
     private List<AddressLabel> addressLabels = new ArrayList<>();
-    
+
     public List<Company> getCompanies() {
         return new ArrayList<>(companies);
     }
-    
+
     public List<Contact> getContacts() {
         return new ArrayList<>(contacts);
     }
-    
+
     public List<MandatorMetadata> getMandatorMetadata() {
         return new ArrayList<>(mandatorMetadata);
     }
@@ -209,11 +209,11 @@ public class Customer implements Serializable {
     public void add(CustomerFlag customerFlag) {
         if ( customerFlag != null ) flags.add(customerFlag);
     }
-    
+
     public void remove(CustomerFlag customerFlag) {
         flags.remove(customerFlag);
     }
-    
+
     public void clearFlags() {
         flags.clear();
     }
@@ -326,11 +326,11 @@ public class Customer implements Serializable {
             sc.setSex(contacts.get(0).getSex());
             sc.setSource(source);
             sc.setComment(comment);
-            
-            return Optional.of(sc); 
+
+            return Optional.of(sc);
         }
         if ( isBussines() ) {
-             SimpleCustomer sc = new SimpleCustomer();
+            SimpleCustomer sc = new SimpleCustomer();
             sc.setId(id);
             sc.setTitle(companies.get(0).getContacts().get(0).getTitle());
             sc.setFirstName(companies.get(0).getContacts().get(0).getFirstName());
@@ -357,40 +357,44 @@ public class Customer implements Serializable {
             sc.setComment(comment);
             sc.setCompanyName(companies.get(0).getName());
             sc.setTaxId(companies.get(0).getTaxId());
-            
-            return Optional.of(sc); 
+
+            return Optional.of(sc);
         }
         throw new RuntimeException("is Simple, but neither consumer nor bussiness. Invaid");
     }
-    
+
     public boolean isSimple() {
         // Für Azubis
         if ( isConsumer() ) {
             if ( contacts.size() > 1
                     || contacts.get(0).getAddresses().size() > 1
-                    || contacts.get(0).getCommunications().stream().map(Communication::getType).filter(t -> !EnumSet.of(EMAIL, PHONE, MOBILE).contains(t)).findAny().isPresent() )
-                return false;
-            
-            return true;
-        } else { // company must not be empty be definiton.
-            if ( companies.size() > 1
-                    || companies.get(0).getAddresses().size() > 1
-                    || companies.get(0).getCommunications().stream().map(Communication::getType).filter(t -> !EnumSet.of(EMAIL, PHONE, MOBILE).contains(t)).findAny().isPresent() )
-                return false;
-            // TODO: More creterias are here.
+                    || contacts.get(0).getCommunications().stream().map(Communication::getType).filter(t -> !EnumSet.of(EMAIL, PHONE, MOBILE).contains(t)).findAny().isPresent() ) {
+                return true;
+            }
 
             return true;
+        } else {
+            // company must not be empty be definiton.
+            if ( companies.size() > 1
+                    || companies.get(0).getAddresses().size() > 1
+                    || companies.get(0).getCommunications().stream().map(Communication::getType).filter(t -> !EnumSet.of(EMAIL, PHONE, MOBILE).contains(t)).findAny().isPresent() ){
+                return true;
+            }
+                
+            // TODO: More creterias are here.
+            return false;
         }
+ 
     }
-    
+
     public boolean isConsumer() {
         return !contacts.isEmpty();
     }
-    
+
     public boolean isBussines() {
         return !companies.isEmpty();
     }
-    
+
     public boolean isVaild() {
         return getViolationMessage() == null;
     }
@@ -400,21 +404,21 @@ public class Customer implements Serializable {
     public String getViolationMessage() {
         return null;
     }
-    
+
     public String toHtml() {
         return toHtml(
                 mandatorMetadata.stream().map(m -> "<li>" + m.toHtml() + "</li>").reduce((u, t) -> u + t).map(s -> "<b>Mandantenspezifische Informationen</b>:<ul>" + s + "</ul>").orElse(""),
                 Optional.ofNullable(comment).map(c -> "<b>Kommentar</b>:<br />" + c).orElse("")
         );
     }
-    
+
     public String toHtml(String matchcode, DefaultCustomerSalesdata defaults) {
-        
+
         StringBuilder sb = new StringBuilder();
         sb.append("<b>Verkaufsinformationen</b> <i>(mit Mandatenstandard)</i>:<ul>");
-        
+
         MandatorMetadata customerExtras = mandatorMetadata.stream().filter(m -> m.getMandatorMatchcode().equals(matchcode)).findFirst().orElse(new MandatorMetadata());
-        
+
         sb.append("<li>Versandkonditionen: ")
                 .append(customerExtras.getShippingCondition() == null ? defaults.getShippingCondition() + " <i>(Standard)</i>" : customerExtras.getShippingCondition())
                 .append("</li>");
@@ -429,22 +433,22 @@ public class Customer implements Serializable {
                         ? defaults.getAllowedSalesChannels().stream().map(SalesChannel::getName).collect(Collectors.toList()) + " <i>(Standard)</i>"
                         : customerExtras.getAllowedSalesChannels().stream().map(SalesChannel::getName).collect(Collectors.toList()))
                 .append("</li>");
-        
+
         sb.append("</ul>");
-        
+
         return toHtml(sb.toString(),
                 Optional.ofNullable(comment).map(c -> "<b>Kommentar</b>:<br />" + c).orElse("")
         );
     }
-    
+
     private String toHtml(String salesRow, String comment) {
         final boolean misc = (!additionalCustomerIds.isEmpty() || source != null || keyAccounter != null);
-        
+
         StringBuilder sb = new StringBuilder();
         sb.append("<table width=\"100%\"><tr>");
         sb.append("<td colspan=2 ><b>Kid: ").append(id).append("&nbsp;-&nbsp;").append(toName()).append("</b></td>");
         sb.append("</tr><tr>");
-        
+
         sb.append("<td valign=top>");
         if ( toPreferedShippingAddress().getType() == INVOICE ) sb.append("<b>Bevorzugte Rechnungs- und Lieferadresse</b><br />");
         else sb.append("<b>Bevorzugte Rechnungsadresse</b><br />");
@@ -454,12 +458,12 @@ public class Customer implements Serializable {
             sb.append(toPreferedShippingAddress().toHtml());
         }
         sb.append("</td>");
-        
+
         int rowSpan = 1;
         if ( !flags.isEmpty() ) rowSpan++;
         if ( misc ) rowSpan++;
         if ( !StringUtils.isBlank(salesRow) ) rowSpan++;
-        
+
         sb.append("<td valign=top rowspan=").append(rowSpan).append(" >");
         sb.append(companies.stream().map(c -> "<li>" + c.toHtml() + "</li>").reduce((t, u) -> t + u).map(s -> "<b>Firmen(n)</b>:<ul>" + s + "</ul>").orElse(""));
         sb.append(contacts.stream().map(c -> "<li>" + c.toHtml() + "</li>").reduce((t, u) -> t + u).map(s -> "<b>Kontakt(e)</b>:<ul>" + s + "</ul>").orElse(""));
@@ -477,11 +481,11 @@ public class Customer implements Serializable {
                 .map(s -> "Weitere Kundennummer(n):<ul>" + s + "</ul>")
                 .orElse(""));
         if ( misc ) sb.append("</td></tr>");
-        
+
         if ( !StringUtils.isBlank(salesRow) ) sb.append("<tr><td valign=top>").append(salesRow).append("</tr></td>");
         if ( !StringUtils.isAllBlank(comment) ) sb.append("<tr><td colspan=2>").append(comment).append("</tr></td>");
         sb.append("</table>");
         return sb.toString();
     }
-    
+
 }
--- ee/persistence/customer/src/test/java/eu/ggnet/dwoss/customer/test/CustomerTest.java
+++ ee/persistence/customer/src/test/java/eu/ggnet/dwoss/customer/test/CustomerTest.java
@@ -3,8 +3,7 @@ package eu.ggnet.dwoss.customer.test;
 import org.junit.Test;
 
 import eu.ggnet.dwoss.customer.assist.gen.CustomerGenerator;
-import eu.ggnet.dwoss.customer.entity.Company;
-import eu.ggnet.dwoss.customer.entity.Customer;
+import eu.ggnet.dwoss.customer.entity.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,10 +28,41 @@ public class CustomerTest {
             Company tempCompany = gen.makeCompany();
             customer.add(tempCompany);
         }
-        assertThat(customer.isBussines()).as("Customer is a Bussines").isTrue();
+        assertThat(customer.isBussines()).as("Customer is a BussinesCustomer").isTrue();
     }
-    
-    
-    
+
+    @Test
+    public void testIsConsumer() {
+        CustomerGenerator gen = new CustomerGenerator();
+        Customer customer = gen.makeCustomer();
+        if ( customer.getContacts().isEmpty() ) {
+            Contact tempContact = gen.makeContact();
+            customer.add(tempContact);
+        }
+        assertThat(customer.isConsumer()).as("Customer is a ConsumerCustomer").isTrue();
+    }
+
+    @Test
+    public void testIsSimpler() {
+        CustomerGenerator gen = new CustomerGenerator();
+        Customer bussineCustomer = gen.makeCustomer();
+        bussineCustomer.add(gen.makeCompany());
+
+        assertThat(bussineCustomer.isBussines()).as("Customer is a BussinesCustomer").isTrue();
+        assertThat(bussineCustomer.isSimple()).as("Bussnis Customer is possible convert to SimpleCustomer").isTrue();
+
+        Customer consumerCustomer = gen.makeCustomer();
+        consumerCustomer.add(gen.makeCompany());
+        
+        assertThat(consumerCustomer.isConsumer()).as("Customer is a ConsumerCustomer").isTrue();
+        assertThat(bussineCustomer.isSimple()).as("Consumer Customer is possible convert to SimpleCustomer").isTrue();
+    }
+
+    @Test
+    public void testToSimple() {
+        CustomerGenerator gen = new CustomerGenerator();
+        Customer customer = gen.makeCustomer();        
+        assertThat(customer.toSimple()).as("Customer convert to SimpleCustomer and is not null").isNotNull();
+    }
 
 }
