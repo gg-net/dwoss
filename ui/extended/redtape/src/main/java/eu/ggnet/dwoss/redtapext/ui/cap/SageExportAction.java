4464e91f77f33fdbeb9eb2856b7bee150d5395f4
Jens Papenhagen <jenspapenhagen@users.noreply.github.com>
2018-02-15 13:03:11 +0100

    Saft OptimizedCreditMemoReportAction [DWOSS-224]

--- ui/extended/redtape/src/main/java/eu/ggnet/dwoss/redtapext/ui/cap/SageExportAction.java
+++ ui/extended/redtape/src/main/java/eu/ggnet/dwoss/redtapext/ui/cap/SageExportAction.java
@@ -18,23 +18,15 @@ package eu.ggnet.dwoss.redtapext.ui.cap;
 
 import java.awt.event.ActionEvent;
 
-import javafx.geometry.Insets;
-import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
-
 import eu.ggnet.dwoss.configuration.GlobalConfig;
 import eu.ggnet.dwoss.redtapext.ee.sage.SageExporter;
 import eu.ggnet.saft.*;
 import eu.ggnet.saft.core.auth.AccessableAction;
 
-import lombok.AllArgsConstructor;
 
-import java.time.*;
-import java.util.Date;
+import eu.ggnet.dwoss.util.DateRangeChooserView;
 
 import static eu.ggnet.dwoss.rights.api.AtomicRight.EXPORT_DOCUMENTS_FOR_SAGE_IN_XML;
-import static javafx.scene.control.ButtonType.CANCEL;
-import static javafx.scene.control.ButtonType.OK;
 
 /**
  * Action to create the GsOfficeXml.
@@ -47,39 +39,12 @@ public class SageExportAction extends AccessableAction {
         super(EXPORT_DOCUMENTS_FOR_SAGE_IN_XML);
     }
 
-    @AllArgsConstructor
-    public static class RangeOfDates {
-
-        public LocalDate start;
-
-        public LocalDate end;
-    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Ui.build().title("Bitte Zeitraum angeben").dialog().eval(() -> {
-            Dialog<RangeOfDates> dialog = new Dialog<>();
-            GridPane grid = new GridPane();
-            grid.setHgap(10);
-            grid.setVgap(10);
-            grid.setPadding(new Insets(20, 150, 10, 10));
-
-            DatePicker startDate = new DatePicker();
-            DatePicker endDate = new DatePicker();
-
-            grid.add(new Label("Bitte Zeitraum angeben: "), 0, 0);
-            grid.add(startDate, 1, 0);
-            grid.add(endDate, 1, 1);
-
-            dialog.getDialogPane().setContent(grid);
-            dialog.getDialogPane().getButtonTypes().addAll(OK, CANCEL);
-
-            RangeOfDates result = new RangeOfDates(startDate.getValue(), endDate.getValue());
-            dialog.setResult(result);
-            return dialog;
-        }).ifPresent(r -> {
+       Ui.build().fx().eval(() -> new DateRangeChooserView()).ifPresent(r -> {
            Ui.progress().title("Sage Export")
-                    .call(() -> Dl.remote().lookup(SageExporter.class).toXml(Date.from(r.start.atStartOfDay(ZoneId.systemDefault()).toInstant()), Date.from(r.end.atStartOfDay(ZoneId.systemDefault()).toInstant())).toFile(GlobalConfig.APPLICATION_PATH_OUTPUT));
+                    .call(() -> Dl.remote().lookup(SageExporter.class).toXml(r.getStartAsDate(), r.getEndAsDate()).toFile(GlobalConfig.APPLICATION_PATH_OUTPUT));
         });
 
     }
