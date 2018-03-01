976cca50898cf930de3daf86adaaa19d34209fd6
jacob.weinhold <jacob.weinhold@gg-net.de>
2018-03-01 17:45:57 +0100

    DWOSS-231 Enhance Mandator for all Manadator Data in DwOss EE Web Misc

---
+++ ee/web/misc/src/main/java/eu/ggnet/dwoss/misc/web/MandatorController.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2018 GG-Net GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.ggnet.dwoss.misc.web;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.ggnet.dwoss.mandator.Mandators;
+import eu.ggnet.dwoss.mandator.api.value.Ledger;
+import eu.ggnet.dwoss.mandator.api.value.ReceiptCustomers.Key;
+import eu.ggnet.dwoss.mandator.api.value.ShippingTerms.ConditionValue;
+import eu.ggnet.dwoss.rules.*;
+
+import lombok.Getter;
+
+/**
+ *
+ * @author jacob.weinhold
+ */
+@Getter
+@Named
+@ViewScoped
+public class MandatorController implements Serializable {
+
+    private final static Logger LOG = LoggerFactory.getLogger(MandatorController.class);
+
+    @EJB
+    private Mandators mandator;
+
+    private TreeNode root;
+
+    public List<TradeName> getAllowedBrands() {
+        return mandator.loadContractors().allowedBrands().stream().collect(Collectors.toList());
+    }
+
+    public List<TradeName> getAllContractors() {
+        return mandator.loadContractors().all().stream().collect(Collectors.toList());
+    }
+
+    // ShippingTerms
+    public List<ConditionValue> getShippingConditionValues() {
+        List<ConditionValue> conditions = new ArrayList<>();
+        for (ShippingCondition value : ShippingCondition.values()) {
+
+            if ( mandator.loadShippingTerms().get(value).isPresent() )
+                conditions.add(mandator.loadShippingTerms().get(value).get());
+
+        }
+        return conditions;
+    }
+
+    public List<Map.Entry<Long, DocumentType>> getSpecialSystemCustomers() {
+        return new ArrayList<>(mandator.loadSystemCustomers().getSpecialCustomers().entrySet());
+
+    }
+
+    public List<Map.Entry<Key, Long>> getReceiptCustomers() {
+        return new ArrayList<>(mandator.loadReceiptCustomers().getReceiptCustomers().entrySet());
+    }
+
+    @PostConstruct
+    public void init() {
+        root = new DefaultTreeNode("Root", null);
+
+        Map<PositionType, TreeNode> levelOneNodes = new HashMap<>();
+        Map<PositionType, Map<TaxType, TreeNode>> levelTwoNodes = new HashMap<>();
+
+        List<PositionType> posTypes = Arrays.<PositionType>asList(PositionType.values());
+        List<TaxType> taxTypes = Arrays.<TaxType>asList(TaxType.values());
+
+        for (PositionType posType : PositionType.values()) {
+            for (TaxType taxType : TaxType.values()) {
+                Optional<Ledger> opt = mandator.loadPostLedger().get(posType, taxType);
+                if ( opt.isPresent() ) {
+
+                    LOG.debug("Tax" + taxType + "  Pos: "
+                            + posType + "  Ledger " + opt.get());
+
+                    if ( !levelOneNodes.containsKey(posType) )
+                        levelOneNodes.put(posType, new DefaultTreeNode(posType.toString(), root));
+
+                    if ( !levelTwoNodes.containsKey(posType) ) {
+                        levelTwoNodes.put(posType, new EnumMap<TaxType, TreeNode>(TaxType.class));
+                        levelTwoNodes.get(posType).put(taxType, new DefaultTreeNode(taxType, levelOneNodes.get(posType)));
+                    } else if ( !levelTwoNodes.get(posType).containsKey(taxType) ) {
+                        levelTwoNodes.get(posType).put(taxType, new DefaultTreeNode(taxType, levelOneNodes.get(posType)));
+                    }
+
+                    levelTwoNodes.get(posType).get(taxType).getChildren().add(new DefaultTreeNode(opt.get()));
+                }
+
+            }
+        }
+    }
+
+}
--- ee/web/misc/src/main/java/eu/ggnet/dwoss/misc/web/stub/MandatorSupportStub.java
+++ ee/web/misc/src/main/java/eu/ggnet/dwoss/misc/web/stub/MandatorSupportStub.java
@@ -16,12 +16,19 @@
  */
 package eu.ggnet.dwoss.misc.web.stub;
 
-import java.util.HashMap;
-
 import javax.annotation.ManagedBean;
 import javax.enterprise.inject.Produces;
 
+import eu.ggnet.dwoss.mandator.api.value.ShippingTerms.ConditionValue;
 import eu.ggnet.dwoss.mandator.api.value.*;
+import eu.ggnet.dwoss.rules.DocumentType;
+import eu.ggnet.dwoss.rules.ShippingCondition;
+import eu.ggnet.dwoss.util.MapBuilder;
+
+import static eu.ggnet.dwoss.rules.PositionType.*;
+import static eu.ggnet.dwoss.rules.ReceiptOperation.*;
+import static eu.ggnet.dwoss.rules.TaxType.*;
+import static eu.ggnet.dwoss.rules.TradeName.*;
 
 /**
  *
@@ -30,16 +37,74 @@ import eu.ggnet.dwoss.mandator.api.value.*;
 @ManagedBean
 public class MandatorSupportStub {
 
-    @Produces
-    public static ReceiptCustomers c = new ReceiptCustomers(new HashMap<>());
+    private final static Ledger L_8401_SONDERPOSTEN_UNIT = new Ledger(8401, "Sonderposten Geräte");
+
+    public final static Ledger L_8415_HW_SW_STORE = new Ledger(8415, "Hardware/Software/Kleinteile Store");
+
+    private final static Ledger L_8406_VERSANDKOSTEN = new Ledger(8406, "Versandkosten");
+
+    public final static Ledger L_8418_ACER_TWO_YEAR_POST_LEDGER = new Ledger(8418, "Acer Garantieerweiterung");
+
+    public final static Ledger L_8403_DL_TECHNIK = new Ledger(8403, "Dientsleistung Technik");
+
+    public final static Ledger L_8404_ACER_REPARATUR = new Ledger(8404, "Acer Reparatur Abrechnung");
 
     @Produces
-    public static SpecialSystemCustomers sc = new SpecialSystemCustomers(new HashMap<>());
+    public static ReceiptCustomers c = ReceiptCustomers.builder()
+            .put(ACER, REPAIR, 3)
+            .put(ACER, MISSING_PARTS, 4)
+            .put(ACER, INTERNAL_REWORK, 5)
+            .put(OTTO, REPAIR, 15)
+            .put(OTTO, MISSING_PARTS, 17)
+            .put(OTTO, INTERNAL_REWORK, 18)
+            .put(ONESELF, REPAIR, 21)
+            .put(ONESELF, MISSING_PARTS, 22)
+            .put(ONESELF, INTERNAL_REWORK, 23)
+            .build();
 
     @Produces
-    public static ShippingTerms st = new ShippingTerms(new HashMap<>());
+    public static SpecialSystemCustomers sc = new SpecialSystemCustomers(new MapBuilder<Long, DocumentType>()
+            .put(34L, DocumentType.CAPITAL_ASSET).put(40L, DocumentType.RETURNS).toHashMap());
 
     @Produces
-    public static PostLedger pl = new PostLedger();
+    public static ShippingTerms st = new ShippingTerms(new MapBuilder<ShippingCondition, ConditionValue>()
+            .put(ShippingCondition.DEFAULT, new ConditionValue("6€/Gerät min. 10€", 10, 6))
+            .put(ShippingCondition.DEALER_ONE, new ConditionValue("5€/Gerät", 5, 5))
+            .put(ShippingCondition.DEALER_TWO, new ConditionValue("6€/Gerät", 6, 6))
+            .toHashMap());
+
+    @Produces
+    public static PostLedger pl = new PostLedger(
+            PostLedger.add()
+                    .positionTypes(UNIT)
+                    .taxTypes(GENERAL_SALES_TAX_DE_SINCE_2007)
+                    .primaryLedger(L_8401_SONDERPOSTEN_UNIT),
+            PostLedger.add()
+                    .positionTypes(UNIT)
+                    .taxTypes(UNTAXED)
+                    .primaryLedger(8431, "GGNet Sonderposten Geräte ohne Ust."),
+            PostLedger.add()
+                    .positionTypes(UNIT, SERVICE, PRODUCT_BATCH, SHIPPING_COST, UNIT_ANNEX)
+                    .taxTypes(REVERSE_CHARGE)
+                    .primaryLedger(8337, "Reverse Charge"),
+            PostLedger.add()
+                    .positionTypes(SERVICE)
+                    .taxTypes(GENERAL_SALES_TAX_DE_SINCE_2007)
+                    .primaryLedger(8414, "Dienstleistung Store")
+                    .alternativeLedger(L_8401_SONDERPOSTEN_UNIT)
+                    .alternativeLedger(L_8403_DL_TECHNIK)
+                    .alternativeLedger(L_8404_ACER_REPARATUR)
+                    .alternativeLedger(L_8406_VERSANDKOSTEN)
+                    .alternativeLedger(L_8415_HW_SW_STORE),
+            PostLedger.add()
+                    .positionTypes(PRODUCT_BATCH)
+                    .taxTypes(GENERAL_SALES_TAX_DE_SINCE_2007)
+                    .primaryLedger(L_8415_HW_SW_STORE)
+                    .alternativeLedger(L_8418_ACER_TWO_YEAR_POST_LEDGER),
+            PostLedger.add()
+                    .positionTypes(SHIPPING_COST)
+                    .taxTypes(GENERAL_SALES_TAX_DE_SINCE_2007)
+                    .primaryLedger(L_8406_VERSANDKOSTEN)
+    );
 
 }
