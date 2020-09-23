14cf3a75cffa55520b1a63391d37b4aa786420ac
Mirko <mirko.schulze@gg-net.de>
2020-09-23 17:40:46 +0200

    [DWPRO-17] Rework RightsAgent
    Implemented create() and made test.

---
+++ ee/persistence/rights/src/main/java/eu/ggnet/dwoss/rights/ee/UserAgentBean.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2020 GG-Net GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.ggnet.dwoss.rights.ee;
+
+import java.util.Objects;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+
+import eu.ggnet.dwoss.core.system.autolog.AutoLogger;
+import eu.ggnet.dwoss.core.system.persistence.AbstractAgentBean;
+import eu.ggnet.dwoss.rights.api.AtomicRight;
+import eu.ggnet.dwoss.rights.ee.assist.Rights;
+import eu.ggnet.dwoss.rights.ee.eao.OperatorEao;
+import eu.ggnet.dwoss.rights.ee.entity.Operator;
+
+/**
+ * Implementation of {@link UserAgent}.
+ *
+ * @author mirko.schulze
+ */
+@Stateless
+@LocalBean
+public class UserAgentBean extends AbstractAgentBean implements UserAgent {
+
+    @Inject
+    @Rights
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    @AutoLogger
+    @Override
+    public void create(String username) {
+        Objects.requireNonNull(username, "Submitted username is null.");
+        if ( username.isBlank() ) {
+            throw new IllegalArgumentException("Submitted username is blank.");
+        }
+        em.persist(new Operator(username));
+    }
+
+    @Override
+    public void updateUsername(long userId, String username) throws EntityNotFoundException, NullPointerException, IllegalArgumentException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updatePassword(long userId, byte[] password) throws EntityNotFoundException, NullPointerException, IllegalArgumentException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateQuickLoginkey(long userId, int quickLoginKey) throws EntityNotFoundException, IllegalArgumentException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(long userId) throws EntityNotFoundException, IllegalStateException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addRight(long userId, AtomicRight right) throws EntityNotFoundException, NullPointerException, IllegalArgumentException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeRight(long userId, AtomicRight right) throws EntityNotFoundException, NullPointerException, IllegalArgumentException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addGroup(long userId, long groupId) throws EntityNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeGroup(long userId, long groupId) throws EntityNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @AutoLogger
+    @Override
+    public Operator findByName(String username) throws IllegalArgumentException, NullPointerException {
+        Objects.requireNonNull(username, "Submitted username is null.");
+        if ( username.isBlank() ) {
+            throw new IllegalArgumentException("Submitted username is blank.");
+        }
+        Operator user = new OperatorEao(em).findByUsername(username);
+        user.fetchEager();
+        return user;
+    }
+
+}
---
+++ ee/persistence/rights/src/test/java/eu/ggnet/dwoss/rights/itest/UserAgentIT.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2020 GG-Net GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.ggnet.dwoss.rights.itest;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.UserTransaction;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import eu.ggnet.dwoss.rights.api.AtomicRight;
+import eu.ggnet.dwoss.rights.ee.UserAgent;
+import eu.ggnet.dwoss.rights.ee.assist.Rights;
+import eu.ggnet.dwoss.rights.ee.entity.Operator;
+import eu.ggnet.dwoss.rights.itest.support.ArquillianProjectArchive;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Arquillian.class)
+public class UserAgentIT extends ArquillianProjectArchive {
+
+    @Inject
+    @Rights
+    private EntityManager em;
+
+    @EJB
+    private UserAgent agent;
+
+    @Inject
+    private UserTransaction utx;
+
+    private final static String NAME = "Test User";
+
+    private final static String UPDATED_NAME = "User Test";
+    
+    private static final byte[] PASSWORD = {1,0,1,0};
+    
+    private static final byte[] UPDATED_PASSWORD = {0,1,0,1};
+    
+    private static final int QUICK_LOGIN_KEY = 0;
+    
+    private static final int UPDATED_QUICK_LOGIN_KEY = 1;
+
+    private final static AtomicRight R = AtomicRight.CHANGE_TAX;
+
+    @After
+    public void tearDown() throws Exception {
+        utx.begin();
+        em.joinTransaction();
+
+        if ( em == null ) {
+            return;
+        }
+        em.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+
+        em.createNativeQuery("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA='PUBLIC'")
+                .getResultList()
+                .stream().forEachOrdered((table) -> {
+                    em.createNativeQuery("TRUNCATE TABLE " + table).executeUpdate();
+                });
+
+        em.createNativeQuery("SELECT SEQUENCE_NAME FROM INFORMATION_SCHEMA.SEQUENCES WHERE SEQUENCE_SCHEMA='PUBLIC'")
+                .getResultList()
+                .stream().forEachOrdered((sequence) -> {
+                    em.createNativeQuery("ALTER SEQUENCE " + sequence + " RESTART WITH 1").executeUpdate();
+                });
+        em.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+
+        utx.commit();
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        agent.create(NAME);
+
+        assertThat(agent.count(Operator.class)).as("One existing User").isEqualTo(1);
+        Operator found = agent.findByName(NAME);
+        assertThat(found).as("User exists").isNotNull();
+    }
+}
