08b355f8d4e005106f54cb9a1b097bfc7f054b20
AVATARMKVII <avatarmkvii@hotmail.com>
2020-11-18 10:43:10 +0100

    [DWOSS-361] Redesign Rights Api
    Imeplemented updatePassword.

--- ee/persistence/rights/src/main/java/eu/ggnet/dwoss/rights/ee/UserApiBean.java
+++ ee/persistence/rights/src/main/java/eu/ggnet/dwoss/rights/ee/UserApiBean.java
@@ -16,6 +16,9 @@
  */
 package eu.ggnet.dwoss.rights.ee;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 import javax.ejb.LocalBean;
@@ -24,6 +27,7 @@ import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,7 +36,6 @@ import eu.ggnet.dwoss.core.system.autolog.AutoLogger;
 import eu.ggnet.dwoss.rights.api.AtomicRight;
 import eu.ggnet.dwoss.rights.api.User;
 import eu.ggnet.dwoss.rights.api.UserApi;
-import eu.ggnet.dwoss.rights.api.Group;
 import eu.ggnet.dwoss.rights.ee.assist.Rights;
 import eu.ggnet.dwoss.rights.ee.entity.Operator;
 import eu.ggnet.dwoss.rights.ee.entity.Persona;
@@ -54,46 +57,47 @@ import eu.ggnet.dwoss.rights.api.PreAuthenticationHook;
 @AutoLogger
 public class UserApiBean implements UserApi {
 
+    private final static Logger L = LoggerFactory.getLogger(UserApiBean.class);
+
     @Inject
     @Rights
     private EntityManager em;
-    
-    private final static Logger L = LoggerFactory.getLogger(UserApiBean.class);
 
-    
     @Inject
     private Instance<PreAuthenticationHook> service;
-    
+
     @Override
     public User authenticate(String username, char[] password) throws UserInfoException {
-        L.info("login(user={}, password=xxxxxxx) requested", username);
+        L.info("login({}, xxxxxxx) called", username);
         //find users by Username
-        if ( username == null ) throw new UserInfoException("Kein Username angegeben");
-        if ( password == null || password.length == 0 ) throw new UserInfoException("Kein Password angegeben");
-        
+        if ( username == null || username.isBlank() ) throw new UserInfoException("Kein Nutzername angegeben.");
+        if ( password == null || password.length == 0 ) throw new UserInfoException("Kein Passwort angegeben.");
+
         Operator op = new JPAQuery<Operator>(em).from(operator).where(operator.username.eq(username)).fetchOne();
-        if ( op == null ) throw new UserInfoException("User " + username + " existiert nicht");
+        if ( op == null ) throw new UserInfoException("Benutzer " + username + " existiert nicht.");
+
         if ( !service.isAmbiguous() && !service.isUnsatisfied() ) {
             if ( service.get().authenticate(username, password) ) {
-                L.info("login(user={}, password=xxxxxxx) via AuthenticationService successful.", username);
-                return findByName(username);
+                L.info("login() via AuthenticationService successful");
+                return op.toApiUser();
             }
         } else {
             if ( op.getPassword() != null && op.getSalt() != null
                     && Arrays.equals(op.getPassword(), PasswordUtil.hashPassword(password, op.getSalt())) ) {
-                L.info("login(user={}, password=xxxxxxx) via internal database successful.", username);
-                return findByName(username);
+                L.info("login() via internal database successful");
+                return op.toApiUser();
             }
-
         }
-        L.warn("login(user={}, password=xxxxxxx) failed.", username);
+        L.warn("login() failed", username);
         throw new UserInfoException("Authentifizierung nicht gelungen!");
     }
 
     @Override
     public int getQuickLoginKey(long id) throws IllegalArgumentException {
+        L.info("getQuickLoginKey({}) called", id);
         Operator user = new JPAQuery<Operator>(em).from(operator).where(operator.id.eq(id)).fetchOne();
         if ( user == null ) {
+            L.info("No User found with id {}.", user, id);
             throw new IllegalArgumentException("No User found with id " + id + ".");
         }
         return user.getQuickLoginKey();
@@ -101,6 +105,7 @@ public class UserApiBean implements UserApi {
 
     @Override
     public User create(String username) throws IllegalArgumentException, NullPointerException {
+        L.info("create({}) called", username);
         Objects.requireNonNull(username, "Submitted username is null.");
         if ( username.isBlank() ) {
             throw new IllegalArgumentException("Submitted username is blank.");
@@ -111,6 +116,7 @@ public class UserApiBean implements UserApi {
 
     @Override
     public User updateUsername(long userId, String username) throws IllegalArgumentException, NullPointerException {
+        L.info("updateUsername({}, {}) called", userId, username);
         Operator user = em.find(Operator.class, userId);
         if ( user == null ) {
             throw new IllegalArgumentException("No User found with userId = " + userId + ".");
@@ -120,11 +126,32 @@ public class UserApiBean implements UserApi {
             throw new IllegalArgumentException("Submitted username is blank.");
         }
         user.setUsername(username);
-        return findById(userId);
+        return user.toApiUser();
+    }
+
+    @Override
+    public User updatePassword(long userId, char[] password) throws IllegalArgumentException, NullPointerException {
+        L.info("updatePassword({}, {}) called", userId, password);
+        Operator user = em.find(Operator.class, userId);
+        if ( user == null ) {
+            throw new IllegalArgumentException("No User found with userId = " + userId + ".");
+        }
+        Objects.requireNonNull(password, "Submitted password is null.");
+        if ( password.length == 0 ) {
+            throw new IllegalArgumentException("Submitted password is empty.");
+        }
+
+        byte[] salt = RandomStringUtils.random(30).getBytes();
+        byte[] hashPassword = PasswordUtil.hashPassword(password, salt);
+
+        user.setPassword(hashPassword);
+        user.setSalt(salt);
+        return user.toApiUser();
     }
 
     @Override
     public User addRight(long userId, AtomicRight right) throws IllegalArgumentException, NullPointerException {
+        L.info("addRight({}, {}) called", userId, right);
         Operator user = em.find(Operator.class, userId);
         if ( user == null ) {
             throw new IllegalArgumentException("No User found with userId = " + userId + ".");
@@ -134,11 +161,12 @@ public class UserApiBean implements UserApi {
             throw new IllegalArgumentException("Submitted Right " + right + " is already granted to User " + user.getUsername() + ".");
         }
         user.add(right);
-        return findById(userId);
+        return user.toApiUser();
     }
 
     @Override
     public User removeRight(long userId, AtomicRight right) throws IllegalArgumentException, NullPointerException {
+        L.info("removeRight({}, {}) called", userId, right);
         Operator user = em.find(Operator.class, userId);
         if ( user == null ) {
             throw new IllegalArgumentException("No User found with userId = " + userId + ".");
@@ -148,11 +176,12 @@ public class UserApiBean implements UserApi {
             throw new IllegalArgumentException("Submitted Right " + right + " was not granted to User " + user.getUsername() + " at all.");
         }
         user.getRights().remove(right);
-        return findById(userId);
+        return user.toApiUser();
     }
 
     @Override
     public User addGroup(long userId, long groupId) throws IllegalArgumentException {
+        L.info("addGroup({}, {}) called", userId, groupId);
         Operator user = em.find(Operator.class, userId);
         if ( user == null ) {
             throw new IllegalArgumentException("No User found with userId " + userId + ".");
@@ -165,11 +194,12 @@ public class UserApiBean implements UserApi {
             throw new IllegalArgumentException("Submitted Group " + group.getName() + " is already associated with User " + user.getUsername() + ".");
         }
         user.getPersonas().add(group);
-        return findById(userId);
+        return user.toApiUser();
     }
 
     @Override
     public User removeGroup(long userId, long groupId) throws IllegalArgumentException {
+        L.info("removeGroup({}, {}) called", userId, groupId);
         Operator user = em.find(Operator.class, userId);
         if ( user == null ) {
             throw new IllegalArgumentException("No User found with userId " + userId + ".");
@@ -182,11 +212,12 @@ public class UserApiBean implements UserApi {
             throw new IllegalArgumentException("Submitted Group " + group.getName() + " wasn't associated with User " + user.getUsername() + " at all.");
         }
         user.getPersonas().remove(group);
-        return findById(userId);
+        return user.toApiUser();
     }
 
     @Override
     public void delete(long userId) throws IllegalArgumentException {
+        L.info("delete({}) called", userId);
         Operator user = em.find(Operator.class, userId);
         if ( user == null ) {
             throw new IllegalArgumentException("No User found with userId = " + userId + ".");
@@ -196,79 +227,36 @@ public class UserApiBean implements UserApi {
 
     @Override
     public User findById(long id) throws IllegalArgumentException {
+        L.info("findById({}) called", id);
         Operator user = new JPAQuery<Operator>(em).from(operator).where(operator.id.eq(id)).fetchOne();
         if ( user == null ) {
             throw new IllegalArgumentException("No User found with id " + id + ".");
         }
-        List<Persona> personas = user.getPersonas();
-        List<Group> groups = new ArrayList<>();
-        personas.forEach(g -> {
-            groups.add(new Group.Builder()
-                    .setId(Optional.of(g.getId()))
-                    .setName(g.getName())
-                    .setOptLock(Optional.of(g.getOptLock()))
-                    .addAllRights(g.getPersonaRights())
-                    .build());
-        });
-        return new User.Builder()
-                .setId(Optional.of(user.getId()))
-                .setUsername(user.getUsername())
-                .setOptLock(Optional.of(user.getOptLock()))
-                .addAllGroups(groups)
-                .addAllRights(user.getRights())
-                .build();
+        User u = user.toApiUser();
+        L.info("findById() returning {}", u);
+        return u;
     }
 
     @Override
     public User findByName(String username) throws IllegalArgumentException, NullPointerException {
+        L.info("findByName({}) called", username);
         Objects.requireNonNull(username, "Username is null.");
         Operator user = new JPAQuery<Operator>(em).from(operator).where(operator.username.eq(username)).fetchOne();
         if ( user == null ) {
             throw new IllegalArgumentException("No User found with name " + username + ".");
         }
-        List<Persona> personas = user.getPersonas();
-        List<Group> groups = new ArrayList<>();
-        personas.forEach(g -> {
-            groups.add(new Group.Builder()
-                    .setId(Optional.of(g.getId()))
-                    .setName(g.getName())
-                    .setOptLock(Optional.of(g.getOptLock()))
-                    .addAllRights(g.getPersonaRights())
-                    .build());
-        });
-        return new User.Builder()
-                .setId(Optional.of(user.getId()))
-                .setUsername(user.getUsername())
-                .setOptLock(Optional.of(user.getOptLock()))
-                .addAllGroups(groups)
-                .addAllRights(user.getRights())
-                .build();
+        User u = user.toApiUser();
+        L.info("findByName() returning {}", u);
+        return u;
     }
 
     @Override
     public List<User> findAll() {
+        L.info("findAll() called");
         List<Operator> operators = new JPAQuery<Operator>(em).from(operator).fetch();
         List<User> users = new ArrayList<>();
-        operators.forEach(u -> {
-            List<Group> groups = new ArrayList<>();
-            u.getPersonas().forEach(g -> {
-                groups.add(new Group.Builder()
-                        .setId(Optional.of(g.getId()))
-                        .setName(g.getName())
-                        .setOptLock(Optional.of(g.getOptLock()))
-                        .addAllRights(g.getPersonaRights())
-                        .build());
-            });
-            users.add(new User.Builder()
-                    .setId(Optional.of(u.getId()))
-                    .setUsername(u.getUsername())
-                    .setOptLock(Optional.of(u.getOptLock()))
-                    .addAllGroups(groups)
-                    .addAllRights(u.getRights())
-                    .build());
-
-        });
+        operators.forEach(u -> users.add(u.toApiUser()));
+        L.info("findAll() returning {}", users);
         return users;
     }
-
 }
