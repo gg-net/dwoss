70e979699659908b3d72220f914de6e4bdeff6e5
Oliver Günther <oliver.guenther@gg-net.de>
2021-04-21 13:24:09 +0200

    [DWPRO-33] Lieferantenartikelnummer hinzugefügt.

--- ee/extended/receipt/src/main/java/eu/ggnet/dwoss/receipt/ee/reporting/AuditReporterOperation.java
+++ ee/extended/receipt/src/main/java/eu/ggnet/dwoss/receipt/ee/reporting/AuditReporterOperation.java
@@ -74,10 +74,43 @@ public class AuditReporterOperation implements AuditReporter {
         List<StockTransaction> rollInTransactions = new StockTransactionEao(sem)
                 .findByTypeAndStatus(StockTransactionType.ROLL_IN, StockTransactionStatusType.PREPARED);
         List<Integer> uuIds = toUniqueUnitIds(rollInTransactions);
-
         List<UniqueUnit> uniqueUnits = new UniqueUnitEao(uuem).findByIds(uuIds);
 
         m.worked(5, "preparing Document");
+        FileJacket fj = createReport(uniqueUnits);
+        m.finish();
+        return fj;
+    }
+
+    /**
+     * Returns an audit report of units which are on a roll in transaction, but not yet rolled in.
+     * <p/>
+     * @return an audit report of units which are on a roll in transaction, but not yet rolled in.
+     */
+    @Override
+    public FileJacket byRange(Date start, Date end) {
+        SubMonitor m = monitorFactory.newSubMonitor("AuditReport", 100);
+        m.message("loading UniqueUnits");
+        m.start();
+        List<UniqueUnit> uniqueUnits = new UniqueUnitEao(uuem).findBetweenInputDates(start, end);
+
+        m.worked(5, "preparing Document");
+        FileJacket fj = createReport(uniqueUnits);
+        m.finish();
+        return fj;
+    }
+
+    private static List<Integer> toUniqueUnitIds(List<StockTransaction> rollInTransactions) {
+        List<Integer> uuIds = new ArrayList<>();
+        for (StockTransaction stockTransaction : rollInTransactions) {
+            for (StockUnit stockUnit : stockTransaction.getUnits()) {
+                uuIds.add(stockUnit.getUniqueUnitId());
+            }
+        }
+        return uuIds;
+    }
+
+    private static FileJacket createReport(List<UniqueUnit> uniqueUnits) {
         List<Object[]> rows = new ArrayList<>();
         for (UniqueUnit uu : uniqueUnits) {
             rows.add(new Object[]{
@@ -107,65 +140,6 @@ public class AuditReporterOperation implements AuditReporter {
         sheet.addBelow(table);
         CCalcDocument document = new TempCalcDocument();
         document.add(sheet);
-        FileJacket fj = new FileJacket("Audit", ".xls", LucidCalc.createWriter(LucidCalc.Backend.XLS).write(document));
-        m.finish();
-        return fj;
-    }
-
-    /**
-     * Returns an audit report of units which are on a roll in transaction, but not yet rolled in.
-     * <p/>
-     * @return an audit report of units which are on a roll in transaction, but not yet rolled in.
-     */
-    @Override
-    public FileJacket byRange(Date start, Date end) {
-        SubMonitor m = monitorFactory.newSubMonitor("AuditReport", 100);
-        m.message("loading UniqueUnits");
-        m.start();
-        List<UniqueUnit> uniqueUnits = new UniqueUnitEao(uuem).findBetweenInputDates(start, end);
-
-        m.worked(5, "preparing Document");
-        List<Object[]> rows = new ArrayList<>();
-        for (UniqueUnit uu : uniqueUnits) {
-            rows.add(new Object[]{
-                uu.getRefurbishId(),
-                uu.getProduct().getGroup().getNote(),
-                uu.getProduct().getPartNo(),
-                uu.getSerial(),
-                ProductFormater.toName(uu.getProduct()),
-                uu.getContractor(),
-                UniqueUnitFormater.toSingleLineAccessories(uu),
-                UniqueUnitFormater.toSingleLineComment(uu),
-                UniqueUnitFormater.toSingleLineInternalComment(uu),
-                uu.getCondition().getNote(),
-                uu.getShipmentLabel(),
-                uu.getProduct().getDescription(),
-                ""
-            });
-        }
-
-        CSheet sheet = new CSheet("Audit");
-        STable table = new STable();
-        table.setHeadlineFormat(new CFormat(BOLD_ITALIC, BLACK, WHITE, CENTER, new CBorder(BLACK)));
-        table.add(new STableColumn("SopoNr", 7)).add(new STableColumn("Warengruppe", 13)).add(new STableColumn("ArtikelNr", 15)).add(new STableColumn("Seriennummer", 27));
-        table.add(new STableColumn("Name", 30)).add(new STableColumn("Lieferant", 12)).add(new STableColumn("Zubehör", 50)).add(new STableColumn("Bemerkung", 50));
-        table.add(new STableColumn("Interne Bemerkung", 30)).add(new STableColumn("Zustand", 12)).add(new STableColumn("Shipment", 12)).add(new STableColumn("Beschreibung", 50));
-        table.setModel(new STableModelList(rows));
-        sheet.addBelow(table);
-        CCalcDocument document = new TempCalcDocument();
-        document.add(sheet);
-        FileJacket fj = new FileJacket("Audit", ".xls", LucidCalc.createWriter(LucidCalc.Backend.XLS).write(document));
-        m.finish();
-        return fj;
-    }
-
-    private List<Integer> toUniqueUnitIds(List<StockTransaction> rollInTransactions) {
-        List<Integer> uuIds = new ArrayList<>();
-        for (StockTransaction stockTransaction : rollInTransactions) {
-            for (StockUnit stockUnit : stockTransaction.getUnits()) {
-                uuIds.add(stockUnit.getUniqueUnitId());
-            }
-        }
-        return uuIds;
+        return new FileJacket("Audit", ".xls", LucidCalc.createWriter(LucidCalc.Backend.XLS).write(document));
     }
 }
